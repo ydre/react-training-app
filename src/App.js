25273cf02a4fce688dca4c95bf1621442b47ba00
david isnard <david isnard>
2020-03-08 12:38:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function getButtonText(){
+  return 'click click';
+}
+
+const App = () => {
+ const buttonText = 'Click me';
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text"/>
+      <button style={{backgroundColor: 'blue', color: 'white'}} >
+        {buttonText}
+        {getButtonText()}
+      </button>
     </div>
   );
 }
