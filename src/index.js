df1e814c20dd27fdd15e87d1dbcfc2c33a6f6af7
david isnard <david isnard>
2020-03-08 21:30:40 +0100

    play with some child and parents

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import faker from 'faker';
+
+import CommentDetail from './CommentDetails';
+import ApprovalCard from './ApprovalCard';
+
+const App = () => {
+  return (
+      <div className="ui container comments">
+        <ApprovalCard>
+          <CommentDetail 
+            author="Sam"  
+            timeAgo="Today at 5:00PM" 
+            comment="blab" 
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail 
+            author="Paul"
+            timeAgo="Today at 4:00PM" 
+            comment="dsfdsf" 
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail 
+            author="Bob" 
+            timeAgo="Yesterday at 7:00PM" 
+            comment="yeyyt" 
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+      </div>
+    );
+};
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
